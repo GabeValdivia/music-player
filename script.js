b9510d07eb8a5219028a8e013570cf3434691ba8
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-20 19:33:49 -0600

    Add event listener to set audio API to play next song when current song ends

--- script.js
+++ script.js
@@ -113,3 +113,6 @@ audio.addEventListener('timeupdate', updateProgress);
 
 // Click on progress bar
 progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
