c89cfa07a3ce6981b091bc43ffadb52a9735a42a
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-20 18:21:04 -0600

    Add needed variable to get needed IDs for play, next, prev, musicContainer, audio, progress, progressContainer, title, cover, songs, and songIndex

---
+++ script.js
@@ -0,0 +1,21 @@
+const musicContainer = document.getElementById('music-container');
+
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+
+// Song title
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of songs
+let songIndex =2;
+
+// Initially load song details into DOM
